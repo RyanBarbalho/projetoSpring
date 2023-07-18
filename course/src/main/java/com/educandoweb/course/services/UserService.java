69d9a1c02fbfb5818d640752b125bdb6893d6b38
RyanBarbalho <rgtbc@ic.ufal.br>
2023-07-18 20:08:57 -0300

    camada de serviço, registro de componentes

---
+++ course/src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+//registrado como componente
+@Service // registra a classe como componente do spring e permite que seja injetado
+         // automaticamente com o autowired
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    // retorna todos os usuarios do banco de dados
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        // optional = container que vai carregar o objeto do tipo user
+        Optional<User> obj = repository.findById(id);
+        return obj.get();// retorna o objeto do tipo user que estiver dentro do optional
+    }
+
+}
